4b1ac6fb3cfba3ff5c2924f61b47c774b47d25fb
Andrew Sturza <asturza@synergemtech.com>
2021-05-04 09:57:56 -0700

    Update structure

--- pytest_api/__init__.py
+++ pytest_api/__init__.py
@@ -1,3 +1,16 @@
 from .api import API
 from .middleware import Middleware
 from .response import Response
+from .specification import Open
+
+
+class Specification(Open):
+    def __init__(self, app=None):
+        self.app = app
+        if None not in {app}:
+            self.init_app(app)
+
+    def init_app(self, app=None):
+        self.app = app
+        self.client = app.test_client()
+        Open.__init__(self, app)
