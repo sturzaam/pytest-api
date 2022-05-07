8bcf8ac3a46860d39bcd5f9329a318206a1ad7cf
Andrew Sturza <sturzaam@gmail.com>
2022-05-06 18:12:20 -0700

    Remove reference ot WSGI from __init__.py

--- pytest_api/__init__.py
+++ pytest_api/__init__.py
@@ -1,10 +1,8 @@
 from .asgi import BEHAVIORS, ASGIMiddleware
 from .schemas import OPEN_API
-from .wsgi import WSGIMiddleware
 
 __all__ = [
     "ASGIMiddleware",
-    "WSGIMiddleware",
     "BEHAVIORS",
     "OPEN_API",
 ]
